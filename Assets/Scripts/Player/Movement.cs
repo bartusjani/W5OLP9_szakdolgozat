e88b3f5b5ab2638405540cfcbe4fc6262c53ee3e
jani <bartusjani004@gmail.com>
2024-11-04 17:32:01 +0100

    jump, turning

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private float horizontal;
+    public float speed = 5f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    void Update()
+    {
+
+        horizontal = Input.GetAxis("Horizontal");
+
+        if(Input.GetButton("Jump") && IsGrounded())
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+
+        Flip();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
