cda683f87ebf212366a6fd7a826a31a77b43d605
jani <bartusjani004@gmail.com>
2024-11-15 16:51:56 +0100

    enemy chase

--- Assets/Scripts/Scorpion/EnemyMovement.cs
+++ Assets/Scripts/Scorpion/EnemyMovement.cs
@@ -7,31 +7,56 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed;
     public int patrolDestination;
 
+    public Transform player;
+    public bool isChasing=false;
+    public float chaseDistance;
+
+
     void Update()
     {
 
-       
-        switch (patrolDestination)
+        if (isChasing)
         {
-            case 0:
-                transform.position = Vector2.MoveTowards(transform.position,patrolPoints[0].position,moveSpeed*Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
-                {
-                    transform.localScale = new Vector3((float)-0.06303474, (float)0.05184435, (float)1.0141);
-                    patrolDestination = 1;
-                }
-                break;
-            case 1:
-                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-                if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
-                {
-                    transform.localScale = new Vector3((float)0.06303474, (float)0.05184435, (float)1.0141);
-                    patrolDestination = 0;
-                }
-                break;
-
-            default:
-                break;
+            if (transform.position.x > player.position.x)
+            {
+                transform.localScale = new Vector3((float)-0.06303474, (float)0.05184435, (float)1.0141);
+                transform.position += Vector3.left*moveSpeed*Time.deltaTime;
+            }
+            else if(transform.position.x < player.position.x)
+            {
+                transform.localScale = new Vector3((float)0.06303474, (float)0.05184435, (float)1.0141);
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
         }
+        else
+        {
+
+            if (Vector2.Distance(transform.position, player.position) < chaseDistance) isChasing = true;
+
+            switch (patrolDestination)
+            {
+                case 0:
+                    transform.position = Vector2.MoveTowards(transform.position,patrolPoints[0].position,moveSpeed*Time.deltaTime);
+                    if (Vector2.Distance(transform.position, patrolPoints[0].position) < .2f)
+                    {
+                        transform.localScale = new Vector3((float)-0.06303474, (float)0.05184435, (float)1.0141);
+                        patrolDestination = 1;
+                    }
+                    break;
+                case 1:
+                    transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+                    if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
+                    {
+                        transform.localScale = new Vector3((float)0.06303474, (float)0.05184435, (float)1.0141);
+                        patrolDestination = 0;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+       
     }
 }
