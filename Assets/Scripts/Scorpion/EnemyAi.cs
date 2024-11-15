a5069fa837b721bdb581dafb1306935ece37673d
jani <bartusjani004@gmail.com>
2024-11-15 16:00:13 +0100

    Enemy attack, player health

---
+++ Assets/Scripts/Scorpion/EnemyAi.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+
+public class EnemyAi : MonoBehaviour
+{
+    public GameObject player;
+    public float speed = 3f;
+    private float horizontal;
+    private bool isFacingRight=true;
+
+    private float distance;
+    public float distanceBetween;
+
+
+
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
