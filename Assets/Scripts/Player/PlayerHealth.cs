05ce89035dce301e4b5a924e46460aac86cdc785
jani <bartusjani004@gmail.com>
2025-01-28 16:57:14 +0100

    player hp bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,13 +4,17 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int health;
+    public HealthBar healthbar;
+
     void Start()
     {
         health = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
     }
     public void TakeDamage(int damage)
     {
         health -= damage;
+        healthbar.setHealth(health);
         if (health <= 0) Destroy(gameObject);
     }
 }
