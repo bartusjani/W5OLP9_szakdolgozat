61414d1faf54d227453aab24b9596490cab186c2
jani <bartusjani004@gmail.com>
2025-02-25 14:22:33 +0100

    Door trigger text

--- Assets/Scripts/Player/Trigger.cs
+++ Assets/Scripts/Player/Trigger.cs
@@ -1,14 +1,33 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour
 {
 
     public GameObject door;
+    public GameObject text;
+
+    void Start()
+    {
+
+        text.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            text.SetActive(true);
             door.SetActive(false);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.SetActive(false);
         }
     }
 }
